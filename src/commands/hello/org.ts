b117bf3ce9988b34d08c8a4a8edac353c7ad6725
Smit Dagli <smit.dagli@smitdag-ltm42q6.internal.salesforce.com>
2022-07-08 12:40:10 -0700

    commit to repo to share with cristian

--- src/commands/hello/org.ts
+++ src/commands/hello/org.ts
@@ -7,8 +7,11 @@
 
 import * as os from 'os';
 import { flags, SfdxCommand } from '@salesforce/command';
-import { Messages, SfdxError } from '@salesforce/core';
-import { AnyJson } from '@salesforce/ts-types';
+import { Messages } from '@salesforce/core';
+
+// const util = require('node:util');
+// const exec = util.promisify(require('node:child_process').exec);
+import { exec, ShellString } from 'shelljs';
 
 // Initialize Messages with the current plugin directory
 Messages.importMessagesDirectory(__dirname);
@@ -35,6 +38,10 @@ export default class Org extends SfdxCommand {
       char: 'f',
       description: messages.getMessage('flags.force'),
     }),
+    customWord: flags.string({
+      char: 'c',
+      description: messages.getMessage('flags.customWord'),
+    }),
   };
 
   // Comment this out if your command does not require an org username
@@ -46,50 +53,19 @@ export default class Org extends SfdxCommand {
   // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
   protected static requiresProject = false;
 
-  public async run(): Promise<AnyJson> {
-    const name = (this.flags.name as string) || 'world';
+  public async run(): Promise<string> {
+    const executablePath: string = '/Users/smit.dagli/GoNodeIntegrationPluginTemplate/plugin-template/dadjoke';
 
-    // this.org is guaranteed because requiresUsername=true, as opposed to supportsUsername
-    const conn = this.org.getConnection();
-    const query = 'Select Name, TrialExpirationDate from Organization';
+    const customWord: string = this.flags.customWord as string;
 
-    // The type we are querying for
-    interface Organization {
-      Name: string;
-      TrialExpirationDate: string;
+    if (customWord.length > 0) {
+      const shellOutput = exec(executablePath + '--term' + customWord);
+      this.ux.log(shellOutput.stdout)
+    } else {
+      //const { stdout, stderr } = await exec(executablePath);
+      //console.log('stdout:', stdout);
+      //console.error('stderr:', stderr);
     }
-
-    // Query the org
-    const result = await conn.query<Organization>(query);
-
-    // Organization will always return one result, but this is an example of throwing an error
-    // The output and --json will automatically be handled for you.
-    if (!result.records || result.records.length <= 0) {
-      throw new SfdxError(messages.getMessage('errorNoOrgResults', [this.org.getOrgId()]));
-    }
-
-    // Organization always only returns one result
-    const orgName: string = result.records[0].Name;
-    const trialExpirationDate = result.records[0].TrialExpirationDate;
-
-    let outputString = `Hello ${name}! This is org: ${orgName}`;
-    if (trialExpirationDate) {
-      const date = new Date(trialExpirationDate).toDateString();
-      outputString = `${outputString} and I will be around until ${date}!`;
-    }
-    this.ux.log(outputString);
-
-    // this.hubOrg is NOT guaranteed because supportsHubOrgUsername=true, as opposed to requiresHubOrgUsername.
-    if (this.hubOrg) {
-      const hubOrgId = this.hubOrg.getOrgId();
-      this.ux.log(`My hub org id is: ${hubOrgId}`);
-    }
-
-    if (this.flags.force && this.args.file) {
-      this.ux.log(`You input --force and a file: ${this.args.file as string}`);
-    }
-
-    // Return an object to be displayed with --json
-    return { orgId: this.org?.getOrgId(), outputString };
+    return '';
   }
 }
